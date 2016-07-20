124a4e6605e4c641db3b1a623e2bfc6c6cb11d71
Mike Roylance <roylance.michael@gmail.com>
2016-07-20 15:51:45 -0600

    added more granular rest tokens

--- api/src/main/java/org/roylance/yaas/enums/UrlTokens.kt
+++ api/src/main/java/org/roylance/yaas/enums/UrlTokens.kt
@@ -3,12 +3,19 @@ package org.roylance.yaas.enums
 object UrlTokens {
     const val Prefix = "/rest"
 
-    const val AdminAllUsers = "/admin/all-users"
-    const val AdminIsAdmin = "/admin/is-admin"
-    const val AdminSetAdmin = "/admin/set-admin"
-    const val AdminRemoveAdmin = "/admin/remove-admin"
-    const val AdminDeleteUser = "/admin/delete-user"
-    const val AdminChangePassword = "/admin/change-password"
+    const val Admin = "/admin"
+    const val AllUsers = "/all-users"
+    const val IsAdmin = "/is-admin"
+    const val SetAdmin = "/set-admin"
+    const val RemoveAdmin = "/remove-admin"
+    const val DeleteUser = "/delete-user"
+    const val ChangePassword = "/change-password"
+    const val AdminAllUsers = "$Admin$AllUsers"
+    const val AdminIsAdmin = "$Admin$IsAdmin"
+    const val AdminSetAdmin = "$Admin$SetAdmin"
+    const val AdminRemoveAdmin = "$Admin$RemoveAdmin"
+    const val AdminDeleteUser = "$Admin$DeleteUser"
+    const val AdminChangePassword = "$Admin$ChangePassword"
 
     const val FullAdminAllUsers = Prefix + AdminAllUsers
     const val FullAdminIsAdmin = Prefix + AdminIsAdmin
@@ -17,12 +24,18 @@ object UrlTokens {
     const val FullAdminDeleteUser = Prefix + AdminDeleteUser
     const val FullAdminChangePassword = Prefix + AdminChangePassword
 
-    const val AuthSave = "/auth/save"
-    const val AuthLogin = "/auth/login"
-    const val AuthAuthenticate = "/auth/authenticate"
-    const val AuthExists = "/auth/exists/{userName}"
-    const val AuthRegister = "/auth/register"
-    const val AuthChangePassword = "/auth/change-password"
+    const val Auth = "/auth"
+    const val Save = "/save"
+    const val Login = "/login"
+    const val Exists = "/exists/{userName}"
+    const val Register = "/register"
+    const val Authenticate = "/authenticate"
+    const val AuthSave = "$Auth$Save"
+    const val AuthLogin = "$Auth$Login"
+    const val AuthAuthenticate = "$Auth$Authenticate"
+    const val AuthExists = "$Auth$Exists"
+    const val AuthRegister = "$Auth$Register"
+    const val AuthChangePassword = "$Auth$ChangePassword"
 
     const val FullAuthSave = Prefix + AuthSave
     const val FullAuthLogin = Prefix + AuthLogin
@@ -31,8 +44,10 @@ object UrlTokens {
     const val FullAuthRegister = Prefix + AuthRegister
     const val FullAuthChangePassword = Prefix + AuthChangePassword
 
-    const val UserDeviceAll = "/user-device/all"
-    const val UserDeviceSave = "/user-device/save"
+    const val UserDevice = "/user-device"
+    const val All = "/all"
+    const val UserDeviceAll = "$UserDevice$All"
+    const val UserDeviceSave = "$UserDevice$Save"
 
     const val FullUserDeviceAll = Prefix + UserDeviceAll
     const val FullUserDeviceSave = Prefix + UserDeviceSave

2ebc87f09464a4512dbecfbd1ca4204184e43584
Mike Roylance <roylance.michael@gmail.com>
2016-11-10 15:41:12 -0700

    updated dependencies

--- python/properties.py
+++ python/properties.py
@@ -1,10 +1,10 @@
-#generated from yaclib on Thu Nov 10 14:41:37 MST 2016
-#Thu Nov 10 14:41:37 MST 2016
+#generated from yaclib on Thu Nov 10 15:38:40 MST 2016
+#Thu Nov 10 15:38:40 MST 2016
 major="0"
 name="api"
 githubUrl="https\://github.com/roylanceMichael/yaas.git"
 author="roylance.michael@gmail.com"
 description="models to interface with the org.roylance.yaas.api system"
 license="MIT"
-minor="139"
+minor="140"
 group="org.roylance.yaas"

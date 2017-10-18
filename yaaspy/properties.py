1505c7ae654c93ebc79eff429f56787a401ca817
Mike Roylance <roylance.michael@gmail.com>
2017-10-17 23:19:13 -0600

    latest changes

--- yaaspy/properties.py
+++ yaaspy/properties.py
@@ -1,10 +1,10 @@
-#generated from yaclib on Tue Oct 17 19:00:41 MDT 2017
-#Tue Oct 17 19:00:41 MDT 2017
+#generated from yaclib on Tue Oct 17 23:14:45 MDT 2017
+#Tue Oct 17 23:14:45 MDT 2017
 major="0"
 name="yaas"
 githubUrl="https\://github.com/roylanceMichael/yaas.git"
 author="roylance.michael@gmail.com"
 description="models to interface with the org.roylance.yaas system"
 license="MIT"
-minor="189"
+minor="191"
 group="org.roylance"
